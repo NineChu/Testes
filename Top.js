7bb0360b06b9d0b90b0baf9b9b09c4d06d7973fe
NineChu <81454674+NineChu@users.noreply.github.com>
2021-08-25 09:06:19 -0300

    Add files via upload

---
+++ Top.js
@@ -0,0 +1,20 @@
+var senha;
+
+function bsenha()
+{
+    senha = prompt("Digite a senha:").toString()
+}
+
+var pag = setInterval(pag, 1);
+function pag()
+{
+    if(senha == "bondia")
+    {
+        alert("https://www.youtube.com/channel/UCK0MUByfOTZBLErrDBjy1Gw")
+        senha = ""
+    }
+    else if(senha == "varolant'-'")
+    {
+        alert("https://ninechu.github.io/butao/")
+    }
+}
